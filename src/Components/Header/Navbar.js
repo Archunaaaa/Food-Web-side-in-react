e0c6ddec74c95bc39c320cbfca073cce5da492d7
archunar <you@example.com>
2024-03-07 11:47:44 +0530

    update

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -28,7 +28,7 @@ const Navbar = () => {
         <div className='navbar-content text-white'>
           <div className='brand-and-toggler flex align-center justify-between'>
             <Link to = "/" className='navbar-brand fw-3 fs-22 flex align-center'>
-              {/* <MdFoodBank /> */}
+              <MdFoodBank />
               <span className='navbar-brand-text fw-7'>eat</span>
             </Link>
             <div className='navbar-btns flex align-center'>
